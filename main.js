ddf76767bcc5cea3908a0309054e2eb25378ba53
Rachel Parris <rachel.parris@gmail.com>
2022-05-19 19:51:16 -0500

    Able to capture food menu options for the API request and recieve the API response.

---
+++ main.js
@@ -0,0 +1,19 @@
+const callRecipeAPI = () => {
+  let meal = document.getElementById('meal').value;
+  let cuisine = document.getElementById('cuisine').value;
+  let protein = document.getElementById('protein').value;
+
+    fetch(`https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=${protein}&app_id=455fdd1b&app_key=%20e4256eb4241155c86b0aa96877050a3b&diet=balanced&cuisineType=${cuisine}&mealType=${meal}&dishType=Main%20course&imageSize=REGULAR&random=true&field=uri&field=label&field=image&field=ingredientLines&field=calories&field=cuisineType&field=mealType&field=dishType`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data.hits[0]);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('submit').addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    callRecipeAPI();
+  });
+});
