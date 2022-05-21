0e98b26614446b5cc7751225d9d39f1963a1f947
Danh Nguyen <dannyng777@gmail.com>
2022-05-21 12:07:03 -0500

    pushing drinks into HTML

--- main.js
+++ main.js
@@ -17,16 +17,38 @@ callDrinkApi = ()=>{
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${drink}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data.drinks[0].idDrink)
-        const drinkID = data.drinks[0].idDrink
-        console.log(drinkID)
+        //get random num to get random drink
+        function getRandomNum(min,max){
+            min = Math.ceil(min)
+            max = Math.floor(max)
+            return Math.floor(Math.random()*(max-min)+min);
+        }
+        // console.log(getRandomNum(0 , data.drinks.length))
+
+        const drinkID = data.drinks[getRandomNum(0,data.drinks.length)].idDrink
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkID}`)
         .then(response => response.json())
         .then(exactDrink =>{
+            let drinkIngName =[exactDrink.drinks[0].strIngredient1,exactDrink.drinks[0].strIngredient2,exactDrink.drinks[0].strIngredient3,exactDrink.drinks[0].strIngredient4,exactDrink.drinks[0].strIngredient5,exactDrink.drinks[0].strIngredient6,exactDrink.drinks[0].strIngredient7,exactDrink.drinks[0].strIngredient8,exactDrink.drinks[0].strIngredient9,exactDrink.drinks[0].strIngredient10,exactDrink.drinks[0].strIngredient11,exactDrink.drinks[0].strIngredient12,exactDrink.drinks[0].strIngredient13,exactDrink.drinks[0].strIngredient14,exactDrink.drinks[0].strIngredient15]
+            let drinkIngMeas = [exactDrink.drinks[0].strMeasure1,exactDrink.drinks[0].strMeasure2,exactDrink.drinks[0].strMeasure3,exactDrink.drinks[0].strMeasure4,exactDrink.drinks[0].strMeasure5,exactDrink.drinks[0].strMeasure6,exactDrink.drinks[0].strMeasure7,exactDrink.drinks[0].strMeasure8,exactDrink.drinks[0].strMeasure9,exactDrink.drinks[0].strMeasure10,exactDrink.drinks[0].strMeasure11,exactDrink.drinks[0].strMeasure12,exactDrink.drinks[0].strMeasure13,exactDrink.drinks[0].strMeasure14,exactDrink.drinks[0].strMeasure15]
+            let drinkIng =[];
+            document.getElementById('drinkIngri').innerText=""
+            for(i=0;i<15;i++){
+                if(drinkIngName[i]==null){
+                    break;
+                }
+                if(drinkIngMeas[i]==null){
+                    drinkIng.push(drinkIngName[i])
+                }
+                drinkIng.push(drinkIngMeas[i]+ " - " + drinkIngName[i])
+                let appendDrinkHtml = document.createElement("li")
+                appendDrinkHtml.innerHTML = drinkIng[i]
+                document.getElementById("drinkIngri").appendChild(appendDrinkHtml)
+            }
             let drinkName = exactDrink.drinks[0].strDrink
             let drinkIns = exactDrink.drinks[0].strInstructions
-            let drinkIngr = exactDrink.drinks[0].strIngredient1 /* need to add more Ingr*/
             let drinkImg = exactDrink.drinks[0].strDrinkThumb
+            document.getElementById('drinkInstructions').innerHTML=drinkIns
             console.log(exactDrink.drinks[0])
         })
     });
@@ -36,7 +58,7 @@ callDrinkApi = ()=>{
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('submit').addEventListener('click', (event) => {
         event.preventDefault();
-  
+        
         callDrinkApi();
         callRecipeApi();
     });
