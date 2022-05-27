40f51e09ed49135d6d6d3487dbc719dece22d6c7
Danh Nguyen <dannyng777@gmail.com>
2022-05-26 20:39:31 -0500

    deleted drink.js

--- drinks.js
+++
@@ -1,7 +0,0 @@
-let drinks = ()=>{
-    fetch('www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin')
-    .then(response => response.json())
-    .then(data => console.log(data.drinks));
-}
-
-drinks();
